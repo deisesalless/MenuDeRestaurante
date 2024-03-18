82b812e67e78cd52a71177e0553f550cba087787
Deise Sales <deisesalles18@gmail.com>
2024-03-18 16:38:35 -0300

    1.0 correção da tipagem dto

---
+++ cardapio/src/main/java/br/com/restaurante/cardapio/dto/FoodResponseDTO.java
@@ -0,0 +1,11 @@
+package br.com.restaurante.cardapio.dto;
+
+import br.com.restaurante.cardapio.entities.Food;
+
+public record FoodResponseDTO(Integer id, String title, String image, Integer price) {
+
+    public FoodResponseDTO(Food food) {
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+
+}
