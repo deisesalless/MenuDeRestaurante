255fbd511533aaddca5fab67e37d1b74d45856c4
Deise Sales <deisesalles18@gmail.com>
2024-03-18 14:16:29 -0300

    inicial

---
+++ cardapio/src/test/java/br/com/restaurante/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
