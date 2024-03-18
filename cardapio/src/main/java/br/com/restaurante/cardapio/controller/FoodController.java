6730cfcfc5eaed2a9b3f7b8c65994180a3fa5985
Deise Sales <deisesalles18@gmail.com>
2024-03-18 16:51:37 -0300

    4.0 finalizado back end

--- cardapio/src/main/java/br/com/restaurante/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/br/com/restaurante/cardapio/controller/FoodController.java
@@ -16,12 +16,14 @@ public class FoodController {
     @Autowired
     private FoodRepository foodRepository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public void saveFood(@RequestBody FoodRequestDTO data) {
         Food foodData = new Food(data);
         foodRepository.save(foodData);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll() {
         List<FoodResponseDTO> foodList = foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
--- cardapio/src/main/java/br/com/restaurante/cardapio/dto/FoodResponseDTO.java
+++ cardapio/src/main/java/br/com/restaurante/cardapio/dto/FoodResponseDTO.java
@@ -5,6 +5,7 @@ import br.com.restaurante.cardapio.entities.Food;
 public record FoodResponseDTO(Integer id, String title, String image, Integer price) {
 
     public FoodResponseDTO(Food food) {
+
         this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
     }
 
